0b67957abde624dc40cd34e4559e306f3a1d712e
wangshengkai <wb-wsk578486@alibaba-inc.com>
2019-07-29 18:28:57 +0800

    wsk fix

--- Netty-unpack/src/main/java/com/pancm/netty/server/NettyServer.java
+++ Netty-unpack/src/main/java/com/pancm/netty/server/NettyServer.java
@@ -9,6 +9,7 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
 import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 
@@ -23,9 +24,13 @@ import java.net.InetSocketAddress;
 * @date 2017年9月20日
  */
 public class NettyServer {
-	private  final static int port=2345;  
-      
-    public void start(){  
+	private  final static int port=2345;
+
+    public static void main(String[] args) throws Exception {
+        new NettyServer().start();
+    }
+
+    public void start(){
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);  
         EventLoopGroup workerGroup = new NioEventLoopGroup();  
         try {  
@@ -33,9 +38,8 @@ public class NettyServer {
                     .childHandler(new ChannelInitializer<SocketChannel>() {  
                         protected void initChannel(SocketChannel ch) throws Exception { 
                         	ChannelPipeline p = ch.pipeline(); 
-                         
                           p.addLast(new LineBasedFrameDecoder(2048));		//字节解码器 ,其中2048是规定一行数据最大的字节数。  用于解决拆包问题
-//                          p.addLast(new FixedLengthFrameDecoder(100));   //定长数据帧的解码器 ，每帧数据100个字节就切分一次。  用于解决粘包问题        
+//                          p.addLast(new FixedLengthFrameDecoder(1000));   //定长数据帧的解码器 ，每帧数据100个字节就切分一次。  用于解决粘包问题
 //                          p.addLast(new DelimiterBasedFrameDecoder(1024,Unpooled.copiedBuffer("~_~".getBytes()))); //固定字符切分解码器 ,会以"~_~"为分隔符。  注意此方法要放到StringDecoder()上面
                             p.addLast(new StringDecoder());     //设置解码器
                             p.addLast(new NettyServerHandler());   //绑定自定义事物
@@ -48,13 +52,9 @@ public class NettyServer {
                
              System.out.println("服务端启动成功，端口为 :" + port );  
              future.channel().closeFuture().sync();  
-        } catch (Exception e) {  
+        } catch (Exception e) {
             bossGroup.shutdownGracefully();  //关闭EventLoopGroup，释放掉所有资源包括创建的线程
             workerGroup.shutdownGracefully();  //关闭EventLoopGroup，释放掉所有资源包括创建的线程
         }  
-    }  
-      
-    public static void main(String[] args) throws Exception {  
-        new NettyServer().start();  
     }  
 }
